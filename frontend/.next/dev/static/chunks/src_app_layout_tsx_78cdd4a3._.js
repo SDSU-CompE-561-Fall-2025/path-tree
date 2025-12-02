ff19e65a1fb84b1e438e2c8645eef2987d4004a5
ParshCodes <20btrse025@jainuniversity.ac.in>
2025-12-01 22:10:08 -0800

    Navbar Fixed

--- frontend/.next/dev/static/chunks/src_app_layout_tsx_78cdd4a3._.js
+++ frontend/.next/dev/static/chunks/src_app_layout_tsx_78cdd4a3._.js
@@ -2,8 +2,8 @@
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
   "static/chunks/[root-of-the-server]__0f0ba101._.css",
-  "static/chunks/node_modules_0303c686._.js",
-  "static/chunks/src_6b91f023._.js"
+  "static/chunks/node_modules_be599eed._.js",
+  "static/chunks/src_20100c05._.js"
 ],
     source: "dynamic"
 });
